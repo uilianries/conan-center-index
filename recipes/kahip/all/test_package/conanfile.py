9f6a5302a772f836348b287520eaf97d7027843a
Uilian Ries <uilianr@jfrog.com>
2026-01-28 09:05:59 +0100

    Simplify kahip recipe
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/kahip/all/test_package/conanfile.py
+++ recipes/kahip/all/test_package/conanfile.py
@@ -1,20 +1,12 @@
 from conan import ConanFile
 from conan.tools.build import can_run
-from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.cmake import cmake_layout, CMake
 import os
-import sys
 
 
 class TestPackageConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
-    generators = "CMakeToolchain", "CMakeDeps", "VirtualRunEnv"
-
-    options  = {
-        "python_module"  : [True, False]
-    }
-    default_options = {
-        "python_module"  : False
-    }
+    generators = "CMakeDeps", "CMakeToolchain"
 
     def layout(self):
         cmake_layout(self)
@@ -26,18 +18,8 @@ class TestPackageConan(ConanFile):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-        
-    @property
-    def _python_interpreter(self):
-        if getattr(sys, "frozen", False):
-            return "python"
-        return sys.executable
 
     def test(self):
         if can_run(self):
-            bin_path = os.path.join(self.cpp.build.bindirs[0], "test_package")
+            bin_path = os.path.join(self.cpp.build.bindir, "test_package")
             self.run(bin_path, env="conanrun")
-        
-            if self.options.python_module:
-                module_path = os.path.join(self.source_folder, "test.py")
-                self.run(f"{self._python_interpreter} \"{module_path}\"", env="conanrun")
