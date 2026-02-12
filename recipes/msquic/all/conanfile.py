c097a4702781b8b6b09616f97d8737c396e3f2a7
Uilian Ries <uilianr@jfrog.com>
2026-02-12 11:54:39 +0100

    Change for msquic
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/msquic/all/conanfile.py
+++ recipes/msquic/all/conanfile.py
@@ -1,7 +1,6 @@
 from conan import ConanFile
 from conan.tools.cmake import CMakeToolchain, CMake, cmake_layout
-from conan.tools.files import copy, rmdir
-from conan.tools.scm import Git
+from conan.tools.files import copy, rmdir, get
 import os
 
 
@@ -19,25 +18,25 @@ class MsQuicConan(ConanFile):
     # Binary configuration
     settings = "os", "compiler", "build_type", "arch"
     options = {"shared": [True, False], "fPIC": [True, False]}
-    default_options = {"shared": True, "fPIC": True}
+    default_options = {"shared": False, "fPIC": True}
     implements = ["auto_shared_fpic"]
 
     def source(self):
-        git = Git(self)
-        git.clone(url="https://github.com/microsoft/msquic.git", target=".")
-        git.checkout(commit=f"v{self.version}")
-        git.run("submodule update --init --recursive")
+        get(self, **self.conan_data["sources"][self.version],
+            destination=self.source_folder, strip_root=True)
+            
+        get(self, **self.conan_data["quictls"][self.version],
+            destination=os.path.join(self.source_folder, "submodules", "quictls"), strip_root=True)
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["QUIC_BUILD_TOOLS"] = False
-        tc.variables["QUIC_BUILD_TEST"] = False
-        tc.variables["QUIC_BUILD_PERF"] = False
-        if self.options.shared:
-            tc.variables["BUILD_SHARED_LIBS"] = True
+        tc.cache_variables["QUIC_BUILD_TOOLS"] = False
+        tc.cache_variables["QUIC_BUILD_TEST"] = False
+        tc.cache_variables["QUIC_BUILD_PERF"] = False
+        tc.cache_variables["QUIC_BUILD_SHARED"] = self.options.shared
         tc.generate()
 
     def build(self):
@@ -51,15 +50,18 @@ class MsQuicConan(ConanFile):
              dst=os.path.join(self.package_folder, "licenses"))
         cmake = CMake(self)
         cmake.install()
-        rmdir(self, os.path.join(self.package_folder, "lib", "cmake"))
-        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
         rmdir(self, os.path.join(self.package_folder, "share"))
 
     def package_info(self):
         self.cpp_info.set_property("cmake_file_name", "msquic")
-        self.cpp_info.set_property("cmake_target_name", "msquic::msquic")
-        self.cpp_info.libs = ["msquic"]
+        
+        self.cpp_info.components["msquic"].set_property("cmake_target_name", "msquic::msquic")
+        self.cpp_info.components["msquic"].libs = ["msquic"]
         if self.settings.os in ["Linux", "FreeBSD"]:
-            self.cpp_info.system_libs = ["pthread", "dl", "m"]
+            self.cpp_info.components["msquic"].system_libs = ["pthread", "dl", "m"]
         if self.settings.os == "Macos":
-            self.cpp_info.frameworks = ["CoreFoundation", "Security"]
+            self.cpp_info.components["msquic"].frameworks = ["CoreFoundation", "Security"]
+
+        if self.options.shared:
+            self.cpp_info.components["platform"].set_property("cmake_target_name", "msquic::platform")
+            self.cpp_info.components["platform"].libs = ["msquic_platform"]
