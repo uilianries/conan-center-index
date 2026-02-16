70ec0f1f5f6420aa47adc35608175cafaf013d89
Uilian Ries <uilianr@jfrog.com>
2026-02-16 10:16:10 +0100

    Use libnetconf2 4.1.2
    Signed-off-by: Uilian Ries <uilianr@jfrog.com>

--- recipes/libnetconf2/all/conanfile.py
+++ recipes/libnetconf2/all/conanfile.py
@@ -1,7 +1,10 @@
 from conan import ConanFile
 from conan.tools.cmake import CMake, CMakeToolchain, cmake_layout, CMakeDeps
-from conan.tools.files import get
-from conan.tools.files import apply_conandata_patches, export_conandata_patches
+from conan.tools.files import get, rmdir, copy, replace_in_file
+from conan.tools.build import build_jobs
+import os
+
+required_conan_version = ">=2.4"
 
 class LibNetconf2Conan(ConanFile):
     name = "libnetconf2"
@@ -17,45 +20,63 @@ class LibNetconf2Conan(ConanFile):
         "shared": False,
         "fPIC": True
     }
+    languages = "C"
+    implements = ["auto_shared_fpic"]
 
     def requirements(self):
-        self.requires("libyang/2.1.148")
+        # libnetconf2/session_client.h:23 #include <libyang/libyang.h>
+        self.requires("libyang/4.2.2", transitive_headers=True)
+        # libnetconf2/session_client.h:30 # include <libssh/libssh.h>
+        self.requires("libssh/[>=0.10.6 <1]", transitive_headers=True)
         self.requires("openssl/[>=1.1 <4]")
-        self.requires("libssh/0.10.6")
-
-    def configure(self):
-        if self.options.shared:
-            self.options.rm_safe("fPIC")
-        self.settings.rm_safe("compiler.cppstd")
-        self.settings.rm_safe("compiler.libcxx")
-
-    def export_sources(self):
-        export_conandata_patches(self)
+        self.requires("libcurl/[>=7.78 <9]")
+        self.requires("libxcrypt/4.4.36")
 
     def layout(self):
         cmake_layout(self, src_folder="src")
 
     def source(self):
         get(self, **self.conan_data["sources"][self.version], strip_root=True)
-        apply_conandata_patches(self)
+        # INFO: Inject find_package to find libxcrypt. Project uses check_library_exists only gets from system paths
+        replace_in_file(self, os.path.join(self.source_folder, "CMakeLists.txt"),
+            "check_library_exists",
+            "find_package(libxcrypt CONFIG REQUIRED)\ncheck_library_exists")
 
     def generate(self):
         tc = CMakeToolchain(self)
-        tc.variables["ENABLE_EXAMPLES"] = False        
+        tc.cache_variables["ENABLE_EXAMPLES"] = False
+        tc.cache_variables["ENABLE_TESTS"] = False
+        tc.cache_variables["ENABLE_PAM"] = False
+        tc.cache_variables["YANG_MODULE_DIR"] = os.path.join(self.package_folder, "res")
+        tc.cache_variables["CMAKE_DISABLE_FIND_PACKAGE_Doxygen"] = True
+        # INFO: Avoid consuming system TLS, use Conan OpenSSL package instead.
+        tc.cache_variables["CMAKE_DISABLE_FIND_PACKAGE_MbedTLS"] = True
         tc.generate()
+
         deps = CMakeDeps(self)
+        deps.set_property("libyang", "cmake_additional_variables_prefixes", ["LIBYANG"])
+        deps.set_property("libssh", "cmake_additional_variables_prefixes", ["LIBSSH"])
         deps.generate()
 
     def build(self):
         cmake = CMake(self)
         cmake.configure()
         cmake.build()
-        cmake.install()
 
     def package(self):
         cmake = CMake(self)
         cmake.install()
+        rmdir(self, os.path.join(self.package_folder, "lib", "pkgconfig"))
 
     def package_info(self):
         self.cpp_info.libs = ["netconf2"]
+        if self.settings.os in ["Linux", "FreeBSD"]:
+            self.cpp_info.system_libs = ["pthread"]
+        self.cpp_info.resdirs = ["res"]
         self.cpp_info.set_property("cmake_file_name", "LibNETCONF2")
+        self.cpp_info.set_property("cmake_additional_variables_prefixes", ["LIBNETCONF2",])
+        self.cpp_info.set_property("cmake_extra_variables", {"LIBNETCONF2_ENABLED_SSH": True, "LIBNETCONF2_ENABLED_TLS": True})
+        self.cpp_info.set_property("pkg_config_custom_content", {
+            "LN2_SCHEMAS_DIR": os.path.join(self.package_folder, "res"),
+            "LN2_MAX_THREAD_COUNT": build_jobs(self)
+        })
